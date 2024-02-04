29bb1be97e0792a9207da7862763efc5db504044
elly1107 <elly1107011@gmail.com>
2024-02-04 23:29:47 +0900

    commit

---
+++ src/variable/ex/OperateEx1.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class OperateEx1 {
+    public static void main(String[] args) {
+        for (int i=1; i<=9; i++){
+            for(int j=1; j<=9; j++)
+                System.out.println(i + " * " + j + " = " + i*j);
+        }
+    }
+}
